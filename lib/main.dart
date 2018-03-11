ee8d90666ac3c6da7f87c77bddde3f9c825c4110
Armel S <armel@rm3l.org>
2018-03-12 00:07:34 +0100

    Add bottom navigation bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,105 +1,209 @@
 import 'package:flutter/material.dart';
-import 'package:english_words/english_words.dart';
 
-void main() => runApp(new MyApp());
+void main() => runApp(new AwesomeDevApp());
 
-class MyApp extends StatelessWidget {
+class AwesomeDevApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
-      title: 'Welcome to Flutter',
+      title: 'Awesome Dev',
       theme: new ThemeData(
-        primaryColor: Colors.white,
-        backgroundColor: Colors.blue,
+        primaryColor: Colors.blue,
+        backgroundColor: Colors.grey,
       ),
-      home: new RandomWords(),
+      home: new AwesomeDev(),
     );
   }
 }
 
-class RandomWords extends StatefulWidget {
+class AwesomeDev extends StatefulWidget {
+  static const String routeName = '/material/bottom_navigation';
+
   @override
-  State<StatefulWidget> createState() => new RandomWordsState();
+  State<StatefulWidget> createState() => new _AwesomeDevState();
 }
 
-class RandomWordsState extends State<RandomWords> {
+class _AwesomeDevState extends State<AwesomeDev> with TickerProviderStateMixin {
 
-  final _suggestions = <WordPair>[];
-  final _biggerFont = const TextStyle(fontSize: 18.0);
+  int _currentIndex = 0;
+  List<NavigationIconView> _navigationViews;
 
-  final _saved = new Set<WordPair>();
+  @override
+  void initState() {
+    super.initState();
+    _navigationViews = <NavigationIconView>[
+      new NavigationIconView(
+        icon: const Icon(Icons.new_releases),
+        title: 'News of the Day',
+//        color: Colors.deepPurple,
+        vsync: this,
+      ),
+      new NavigationIconView(
+        icon: const Icon(Icons.favorite),
+        title: 'Favorites',
+        color: Colors.indigo,
+        vsync: this,
+      ),
+      new NavigationIconView(
+        icon: const Icon(Icons.archive),
+        title: 'Archives',
+        color: Colors.deepOrange,
+        vsync: this,
+      ),
+      new NavigationIconView(
+        icon: const Icon(Icons.label),
+        title: 'Tags',
+        color: Colors.teal,
+        vsync: this,
+      ),
+    ];
+
+    for (NavigationIconView view in _navigationViews) {
+      view.controller.addListener(_rebuild);
+    }
+    _navigationViews[_currentIndex].controller.value = 1.0;
+  }
+
+  @override
+  void dispose() {
+    for (NavigationIconView view in _navigationViews)
+      view.controller.dispose();
+    super.dispose();
+  }
+
+  void _rebuild() {
+    setState(() {
+      // Rebuild in order to animate views.
+    });
+  }
+
+  Widget _buildTransitionsStack() {
+    final List<FadeTransition> transitions = <FadeTransition>[];
+
+    for (NavigationIconView view in _navigationViews) {
+      transitions.add(view.transition(BottomNavigationBarType.fixed, context));
+    }
+
+    // We want to have the newly animating (fading in) views on top.
+    transitions.sort((FadeTransition a, FadeTransition b) {
+      final Animation<double> aAnimation = a.opacity;
+      final Animation<double> bAnimation = b.opacity;
+      final double aValue = aAnimation.value;
+      final double bValue = bAnimation.value;
+      return aValue.compareTo(bValue);
+    });
+
+    return new Stack(children: transitions);
+  }
 
   @override
   Widget build(BuildContext context) {
+    final BottomNavigationBar botNavBar = new BottomNavigationBar(
+      items: _navigationViews
+          .map((NavigationIconView navigationView) => navigationView.item)
+          .toList(),
+      currentIndex: _currentIndex,
+      type: BottomNavigationBarType.fixed,
+      onTap: (int index) {
+        setState(() {
+          _navigationViews[_currentIndex].controller.reverse();
+          _currentIndex = index;
+          _navigationViews[_currentIndex].controller.forward();
+        });
+      },
+    );
+
     return new Scaffold(
       appBar: new AppBar(
-        title: new Text("Startup Name Generator"),
-        actions: <Widget>[
-          new IconButton(icon: new Icon(Icons.list), onPressed: _pushSaved)
-        ],
+        title: const Text('Awesome Dev'),
+//        actions: <Widget>[
+//          new PopupMenuButton<BottomNavigationBarType>(
+//            onSelected: (BottomNavigationBarType value) {
+//              setState(() {
+//                _type = value;
+//              });
+//            },
+//            itemBuilder: (BuildContext context) => <PopupMenuItem<BottomNavigationBarType>>[
+//              const PopupMenuItem<BottomNavigationBarType>(
+//                value: BottomNavigationBarType.fixed,
+//                child: const Text('Fixed'),
+//              ),
+//              const PopupMenuItem<BottomNavigationBarType>(
+//                value: BottomNavigationBarType.shifting,
+//                child: const Text('Shifting'),
+//              )
+//            ],
+//          )
+//        ],
       ),
-      body: _buildSuggestions(),
+      body: new Center(
+          child: _buildTransitionsStack()
+      ),
+      bottomNavigationBar: botNavBar,
+    );
+  }
+}
+
+class NavigationIconView {
+  NavigationIconView({
+    Widget icon,
+    String title,
+    Color color,
+    TickerProvider vsync,
+  }) : _icon = icon,
+        _color = color,
+        _title = title,
+        item = new BottomNavigationBarItem(
+          icon: icon,
+          title: new Text(title),
+          backgroundColor: color,
+        ),
+        controller = new AnimationController(
+          duration: kThemeAnimationDuration,
+          vsync: vsync,
+        ) {
+    _animation = new CurvedAnimation(
+      parent: controller,
+      curve: const Interval(0.5, 1.0, curve: Curves.fastOutSlowIn),
     );
   }
 
-  Widget _buildSuggestions() {
-    return new ListView.builder(
-      padding: const EdgeInsets.all(16.0),
-      itemBuilder: (context, i) {
-        if (i.isOdd) return new Divider();
-        final index = i ~/2;
-        if (index >= _suggestions.length) {
-          _suggestions.addAll(generateWordPairs().take(10));
-        }
-        return _buildRow(_suggestions[index]);
-      },
-    );
-  }
+  final Widget _icon;
+  final Color _color;
+  final String _title;
+  final BottomNavigationBarItem item;
+  final AnimationController controller;
+  CurvedAnimation _animation;
 
-  Widget _buildRow(WordPair pair) {
-    final alreadySaved = _saved.contains(pair);
-    return new ListTile(
-      title: new Text(
-        pair.asPascalCase,
-        style: _biggerFont,
-      ),
-      trailing: new Icon(
-        alreadySaved ? Icons.favorite : Icons.favorite_border,
-        color: alreadySaved ? Colors.red : null,
-      ),
-      onTap: () {
-        setState(() {
-          if (alreadySaved) {
-            _saved.remove(pair);
-          } else {
-            _saved.add(pair);
-          }
-        });
-      },
-    );
-  }
+  FadeTransition transition(BottomNavigationBarType type, BuildContext context) {
+    Color iconColor;
+    if (type == BottomNavigationBarType.shifting) {
+      iconColor = _color;
+    } else {
+      final ThemeData themeData = Theme.of(context);
+      iconColor = themeData.brightness == Brightness.light
+          ? themeData.primaryColor
+          : themeData.accentColor;
+    }
 
-  void _pushSaved() {
-    Navigator.of(context).push(
-      new MaterialPageRoute(builder: (context) {
-        final tiles = _saved.map((pair) {
-          return new ListTile(
-              title: new Text(
-                pair.asPascalCase,
-                style: _biggerFont,
-              )
-          );
-        });
-        final divided = ListTile.divideTiles(tiles: tiles, context: context).toList();
-
-        return new Scaffold(
-          appBar: new AppBar(
-            title: new Text('Saved Suggestions'),
+    return new FadeTransition(
+      opacity: _animation,
+      child: new SlideTransition(
+        position: new Tween<Offset>(
+          begin: const Offset(0.0, 0.02), // Slightly down.
+          end: Offset.zero,
+        ).animate(_animation),
+        child: new IconTheme(
+          data: new IconThemeData(
+            color: iconColor,
+            size: 120.0,
           ),
-          body: new ListView(children: divided),
-        );
-
-      })
+          child: new Semantics(
+            label: 'Placeholder for $_title tab',
+            child: _icon,
+          ),
+        ),
+      ),
     );
   }
 }
