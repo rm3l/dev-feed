836da4a45c15b4dc6157eb81ab6d94db4c445c6f
Armel S <armel@rm3l.org>
2019-05-24 00:26:54 +0200

    [Backend] Delay application startup until a first crawling round is done

--- backend/src/main/kotlin/org/rm3l/devfeed/crawlers/DevFeedFetcherService.kt
+++ backend/src/main/kotlin/org/rm3l/devfeed/crawlers/DevFeedFetcherService.kt
@@ -4,18 +4,16 @@ import org.rm3l.devfeed.dal.DevFeedDao
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Qualifier
-import org.springframework.boot.context.event.ApplicationReadyEvent
-import org.springframework.context.ApplicationListener
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Service
 import java.util.concurrent.CompletableFuture
 import java.util.concurrent.ExecutionException
 import java.util.concurrent.ExecutorService
+import javax.annotation.PostConstruct
 
 @Service
 class DevFeedFetcherService(private val dao: DevFeedDao,
-                            private val crawlers: Collection<DevFeedCrawler>? = null):
-        ApplicationListener<ApplicationReadyEvent> {
+                            private val crawlers: Collection<DevFeedCrawler>? = null) {
 
     companion object {
         @JvmStatic
@@ -34,14 +32,20 @@ class DevFeedFetcherService(private val dao: DevFeedDao,
     @Qualifier("articleExtractorExecutorService")
     private lateinit var articleExtractorExecutorService: ExecutorService
 
-    override fun onApplicationEvent(event: ApplicationReadyEvent) {
+    @PostConstruct
+    fun init() {
         logger.info("ApplicationReady => scheduling crawling tasks...")
-        CompletableFuture.runAsync {
-            triggerRemoteWebsiteCrawling()
-            triggerScreenshotUpdater()
-        }.exceptionally { t ->
-            logger.info(t.message, t)
-            null
+        try {
+            CompletableFuture.runAsync {
+                triggerRemoteWebsiteCrawling()
+                triggerScreenshotUpdater()
+            }.exceptionally { t ->
+                logger.info(t.message, t)
+                null
+            }.join()
+        } catch (e: Exception) {
+            logger.warn("init() could not complete successfully - " +
+                    "will try again later", e)
         }
     }
 
