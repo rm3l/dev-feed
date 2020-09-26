12cc56aaeb8b1c13331d01006b0c48b482ea54de
Armel Soro <armel@rm3l.org>
2020-09-27 01:10:10 +0200

    Bump version to 0.9.1

--- backend/run.sh
+++ backend/run.sh
@@ -2,5 +2,5 @@
 set -eux
 
 docker container rm -f dev-feed-backend >/dev/null 2>&1 || true
-docker container run -d --rm --name dev-feed-backend -p 8080:8080 -p 8081:8081 -t org.rm3l/dev-feed-backend:0.9.0
+docker container run -d --rm --name dev-feed-backend -p 8080:8080 -p 8081:8081 -t org.rm3l/dev-feed-backend:0.9.1
 docker container logs -f dev-feed-backend
