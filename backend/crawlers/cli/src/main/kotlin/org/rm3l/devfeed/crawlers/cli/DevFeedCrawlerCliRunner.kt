e7e59b5cd66a0ee1906a473d581e78242152ad10
Armel Soro <armel@rm3l.org>
2020-10-03 23:45:06 +0200

    Bump version to 1.1.1

--- backend/crawlers/cli/src/main/kotlin/org/rm3l/devfeed/crawlers/cli/DevFeedCrawlerCliRunner.kt
+++ backend/crawlers/cli/src/main/kotlin/org/rm3l/devfeed/crawlers/cli/DevFeedCrawlerCliRunner.kt
@@ -32,7 +32,7 @@ import picocli.CommandLine.ParameterException
 
 @CommandLine.Command(description = ["Fetch articles"],
   mixinStandardHelpOptions = true,
-  version = ["1.1.0"],
+  version = ["1.1.1"],
   subcommands = [DevFeedCrawlerCliRdbms::class])
 class DevFeedCrawlerCliRunner : Runnable {
 
