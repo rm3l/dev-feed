6097b2018f62be9804e967231d533c6b4bc09abf
Armel Soro <armel@rm3l.org>
2021-03-20 00:44:33 +0100

    Fix ITs

--- backend/api/src/test/kotlin/org/rm3l/devfeed/DevFeedApplicationIntegrationTests.kt
+++ backend/api/src/test/kotlin/org/rm3l/devfeed/DevFeedApplicationIntegrationTests.kt
@@ -23,17 +23,14 @@
  */
 package org.rm3l.devfeed
 
-import org.junit.Test
-import org.junit.runner.RunWith
+import org.junit.jupiter.api.Test
 import org.slf4j.LoggerFactory
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.context.annotation.Bean
 import org.springframework.test.context.ActiveProfiles
-import org.springframework.test.context.junit4.SpringRunner
 import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
 
-@RunWith(SpringRunner::class)
 @SpringBootTest
 @ActiveProfiles(value = ["test"])
 class DevFeedApplicationIntegrationTests {
